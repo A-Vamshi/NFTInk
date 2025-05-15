ea370b9230bf35bded85268458b6b9ef4ac88f9c
Vamshi Adimalla <vamshiadimalla@Vamshis-MacBook-Air.local>
2025-05-15 13:17:02 +0530

    Deployed the smart contract and ensured everything's working. Now starting frontend part

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,15 @@
-const hre = require("hardhat");
-const { ethers } = require('hardhat');
+const hre = require('hardhat');
 
 async function main() {
     const NAME = "AI Generated NFT";
     const SYMBOL = "AI_NFT";
-    const COST = ethers.parseUnits("1", "ether");
+    const COST = hre.ethers.parseUnits("1", "ether");
 
     const NFT = await hre.ethers.getContractFactory("NFT");
     const nft = await NFT.deploy(NAME, SYMBOL, COST);
-    await nft.deployed();
+    await nft.waitForDeployment();
 
-    console.log(`NFT contract deployed at ${nft.address}`);
+    console.log(`NFT contract deployed at ${await nft.getAddress()}`);
 }
 
 main().catch((error) => {
